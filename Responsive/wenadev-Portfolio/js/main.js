e7721c69b2d89131f0b17f95c5392c7ad4f523aa
Mawuena <53694230+wenadev@users.noreply.github.com>
2020-11-24 23:11:17 +0100

    Add files via upload

---
+++ Responsive/wenadev-Portfolio/js/main.js
@@ -0,0 +1,157 @@
+//getting hamburger menu, the view element that houses the landing page elements and the clear button for the mobile nav view
+let menu = document.querySelector(".menu");
+let view = document.querySelector(".view");
+
+//getting the navigation elements for the web view
+let navElements = Array.from(document.querySelectorAll(".nav-elements.web"));
+
+//getting the div element that houses the elements of mobile navigation view and the clear button to remove mobile nav view 
+let mobileNav = document.querySelector(".mobile-nav");
+let clear = document.querySelector(".clear");
+
+//getting mobile nav elements
+let mobileNavElements = Array.from(document.querySelectorAll(".nav-elements.mobile"));
+
+//checks the width of the window as it is resized
+let windowResizeEvents = ()=>{
+    mobileNav.style.transition= "0s";
+
+    if(window.innerWidth <= 680){
+        //web navigation hides
+        navElements.forEach((element)=>{
+            element.style.display="none";
+        });
+        
+        menu.addEventListener('click', displayMobileNav);
+    }
+    
+    else if(window.innerWidth > 680){
+        //web navigation comes into view
+        navElements.forEach((element)=>{
+            element.style.display="initial";
+        });
+        removeMobileNav();
+    }
+}
+
+//mobile navigation comes into view
+let displayMobileNav = ()=>{
+    mobileNav.style.animation="slide-left .3s ease-out";
+    view.style.display="none";
+    mobileNav.style.display= "initial";
+
+    mobileNavElements.forEach((mobileNavElement)=>{
+        mobileNavElement.addEventListener('click',()=>{
+            //get href link of relating 'a' tag embedded in 'p' tag
+            let sectionHref = mobileNavElement.dataset.href;
+
+            //close mobile nav
+            removeMobileNav();
+
+          
+            
+           window.scrollTo(0, document.getElementById(`${sectionHref}`).offsetTop)
+        })
+    });
+}
+
+//mobile navigation hides 
+let removeMobileNav = ()=>{
+    mobileNav.style.display= "none";
+    view.style.display="inherit";
+}
+
+window.addEventListener('resize', windowResizeEvents);
+
+//when page reloads
+window.onbeforeunload = windowResizeEvents();
+clear.addEventListener('click', removeMobileNav);
+
+
+
+(filterSelection= (filter) =>{
+    let projectContent = Array.from(document.querySelectorAll(".project-content"));
+
+    //if all is selected, change value of filter to pass the conditionals below
+    if(filter == "all" || filter == "All"){filter = "";}
+
+        for(let i = 0; i < projectContent.length; i++){
+
+            //hide elements that are not selected as a filter
+            projectContent[i].classList.remove("show")
+
+            if(projectContent[i].className.indexOf(filter) > -1) { 
+                //show project based on filter  
+                    projectContent[i].classList.add ("show");
+            }
+        }  
+})();
+
+// highlight active filters on projects
+
+(highlightFilter =()=>{
+    let filterButtons = Array.from(document.querySelectorAll(".filter-button"));
+
+    filterButtons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        let activeButton = document.querySelector(".activee");
+        activeButton.classList.remove("activee");
+        button.classList.add("activee")
+        console.log(button.innerHTML)
+        filterSelection(button.innerHTML);
+    })
+})
+})();
+
+//show all projects
+filterSelection("all")
+
+//make header fixed
+let scrollEvent = ()=>{
+
+    let headerHeight = document.querySelector("header");
+    let headerContent = document.querySelector(".header-content")
+
+    if(Math.round(this.pageYOffset) > headerHeight.offsetHeight ){
+        headerContent.classList.add("blur");
+       // headerContent.classList.add("fixed")
+        headerHeight.classList.add("fixed")
+    }
+    else if(Math.round(this.pageYOffset) < headerHeight.offsetHeight ){
+        headerContent.classList.remove("blur");
+       // headerContent.classList.remove("fixed")
+        headerHeight.classList.remove("fixed")
+    }
+}
+
+window.addEventListener("scroll", scrollEvent);
+
+
+var items = $(".check .project-content");
+    var numItems = items.length;
+    var perPage = 4;
+
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "&laquo;",
+        nextText: "&raquo;",
+        onPageClick: function (pageNumber) {
+            let projectContent = Array.from(document.querySelectorAll(".project-content"));
+
+
+            let filterButtons = Array.from(document.querySelectorAll(".filter-button"));
+
+            filterButtons.forEach(()=>{
+                let activeButton = document.querySelector(".activee").innerHTML;
+                filterSelection(activeButton);
+            });
+
+
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+        }
+    });
